f35aef27bd55018f261617ce888c5935703bb1b8
anish-maharzan <anish.maharzan5@gmail.com>
2024-09-05 09:17:52 +0545

    Added Student, Course and Enrollment domain model

---
+++ Respository.API/Models/Domain/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace Respository.API.Models.Domain
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ Respository.API/Models/Domain/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace Respository.API.Models.Domain
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
