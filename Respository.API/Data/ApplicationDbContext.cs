5a9eaeccd8f346cf9b5334c58918cb9e09dcf283
anish-maharzan <anish.maharzan5@gmail.com>
2024-09-05 09:52:13 +0545

    Add DbContext class, connectionstring and run migration

---
+++ Respository.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Respository.API.Models.Domain;
+
+namespace Respository.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
