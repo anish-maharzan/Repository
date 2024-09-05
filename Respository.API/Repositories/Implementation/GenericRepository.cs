225bbd8cecc8596aa4b2613c738e081e122a26d0
anish-maharzan <anish.maharzan5@gmail.com>
2024-09-05 10:23:42 +0545

    Added GenericRepository interface and implementation class

---
+++ Respository.API/Repositories/Implementation/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Respository.API.Data;
+using Respository.API.Repositories.Interface;
+
+namespace Respository.API.Repositories.Implementation
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _dbset;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            this._context = context;
+            _dbset = _context.Set<T>();
+        }
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbset.FindAsync(id);
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbset.ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbset.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbset.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _dbset.Remove(entity);
+            }
+        }
+    }
+}
